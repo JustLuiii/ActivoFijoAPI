5e9baf8addedde36e13983ad48b2d36356186903
dalvinxo <dalvingamer@gmail.com>
2025-03-10 20:56:15 -0400

    fix: se arreglo entidad empleado

--- server/Models/Empleado.cs
+++ server/Models/Empleado.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ActivoFijoAPI.Models
 {
     public class Empleado
@@ -5,8 +7,11 @@ namespace ActivoFijoAPI.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Cedula { get; set; }
+        [Column("departamento_id")]
         public int DepartamentoId { get; set; }
+        [Column("tipo_persona")]
         public byte TipoPersona { get; set; } // 1: Física, 2: Jurídica
+        [Column("fecha_ingreso")]
         public DateTime FechaIngreso { get; set; }
         public bool Activo { get; set; }
     }
